2d361483d18c7718c84109ddcd158bbe35c942f6
Pathum Egodawatta <pathumego@gmail.com>
2015-10-03 00:39:27 +0100

    Init and primary sketches

---
+++ COPYRIGHT.md
@@ -0,0 +1,2 @@
+Copyright (c) 2015 Pathum Egodawatta (http://mooniak.com)
+
---
+++ scripts/update-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
+cd ../sources/sfd/
+
+fontforge ../../scripts/fontconvert experiment-sinhala-0.sfd --otf
+
+mv *otf ../../tests/fonts
