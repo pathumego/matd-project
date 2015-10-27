0df9cabca0f488422baf730a736f7a6d38fefa40
Pathum Egodawatta <pathumego@gmail.com>
2015-10-27 14:36:31 +0000

    Update

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -2,7 +2,7 @@
 #Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
 cd ../sources/sfd/
 
-fontforge ../../scripts/fontconvert experiment-sinhala-0.sfd --otf
+fontforge ../../scripts/fontconvert experiment-sinhala-1.sfd --otf
 fontforge ../../scripts/fontconvert experiment-narrow-sinhala-0.sfd --otf
 
 
