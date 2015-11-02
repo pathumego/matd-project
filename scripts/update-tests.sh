eb1baf0e5a4ffd42727db739087214ae3eb7f3ea
Pathum Egodawatta <pathumego@gmail.com>
2015-11-02 23:30:14 +0000

    cleanup and speciment update

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -3,6 +3,7 @@
 cd ../sources/sfd/
 
 fontforge ../../scripts/fontconvert experiment-sinhala-1.sfd --otf
+fontforge ../../scripts/fontconvert experiment-thaana-1.sfd --otf
 fontforge ../../scripts/fontconvert experiment-narrow-sinhala-0.sfd --otf
 
 
