7dad9f35f7975adf73db608feffa84c7c6f17849
Pathum Egodawatta <pathumego@gmail.com>
2015-11-12 21:49:04 +0000

    Minor Updates to test

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -2,10 +2,16 @@
 #Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
 cd ../sources/sfd/
 
-fontforge ../../scripts/fontconvert experiment-sinhala-1.sfd --otf
-fontforge ../../scripts/fontconvert experiment-thaana-1.sfd --otf
-fontforge ../../scripts/fontconvert experiment-narrow-sinhala-1.sfd --otf
+fontforge ../../scripts/fontconvert experiment-latin-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-sinhala-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-thaana-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-narrow-sinhala-0.sfd --remove-overlap --otf
 
 
 
 mv *otf ../../tests/fonts
+cd ../../tests/fonts
+git add .
+git commit -m 'Minor Updates to test'
+cp *otf /Library/Application\ Support/Adobe/Fonts
+cp *otf /Users/Pathum/Library/Fonts/
