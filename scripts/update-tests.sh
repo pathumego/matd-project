bccd3ec033708ed5572a0bc4473fc14f4eaa91db
Pathum Egodawatta <pathumego@gmail.com>
2016-02-26 14:34:07 +0000

    Cleanup and organisation

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -3,17 +3,19 @@
 cd ../sources/sfd/
 
 fontforge ../../scripts/fontconvert experiment-latin-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-latin-1.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-latin-italic-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-latin-opt-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-latin-opt-1.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-latin-opt-italic-0.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-sinhala-0.sfd --remove-overlap --ot
 fontforge ../../scripts/fontconvert experiment-sinhala-cursive-0.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-thaana-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-narrow-latin-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-tamil-0.sfd --remove-overlap --otf
 
-
-
+rm ../../tests/fonts/*otf
 mv *otf ../../tests/fonts
 cd ../../tests/fonts
 git add .
 git commit -m 'Minor Updates to test'
 cp *otf /Library/Application\ Support/Adobe/Fonts
-cp *otf /Users/Pathum/Library/Fonts/
