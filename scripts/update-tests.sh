0aaa92966d410037b413f0a5c34a26bd2d54fe71
Pathum Egodawatta <pathumego@gmail.com>
2016-03-15 18:19:30 +0000

    meta updates

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -5,13 +5,18 @@ cd ../sources/sfd/
 fontforge ../../scripts/fontconvert experiment-latin-0.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-latin-1.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-latin-italic-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-latin-cursive-0.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-latin-opt-0.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-latin-opt-1.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-latin-opt-italic-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-latin-opt-2.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-sinhala-0.sfd --remove-overlap --ot
 fontforge ../../scripts/fontconvert experiment-sinhala-cursive-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert experiment-sinhala-opt-0.sfd --remove-overlap --ot
 fontforge ../../scripts/fontconvert experiment-thaana-0.sfd --remove-overlap --otf
 fontforge ../../scripts/fontconvert experiment-tamil-0.sfd --remove-overlap --otf
+fontforge ../../scripts/fontconvert Greek-Regular.sfd --remove-overlap --otf
+
 
 rm ../../tests/fonts/*otf
 mv *otf ../../tests/fonts
