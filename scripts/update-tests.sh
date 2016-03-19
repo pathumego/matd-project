f751da5e4fe839090f33ced9e99be33acb7a32bd
Pathum Egodawatta <pathumego@gmail.com>
2016-03-19 23:20:26 +0000

    A bit more intelligent test generator

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 #Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
-cd ../sources/sfd/
-
-fontforge ../../scripts/fontconvert experiment-latin-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-latin-1.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-latin-italic-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-latin-cursive-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-latin-opt-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-latin-opt-1.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-latin-opt-italic-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-latin-opt-2.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-sinhala-0.sfd --remove-overlap --ot
-fontforge ../../scripts/fontconvert experiment-sinhala-cursive-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-sinhala-opt-0.sfd --remove-overlap --ot
-fontforge ../../scripts/fontconvert experiment-thaana-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert experiment-tamil-0.sfd --remove-overlap --otf
-fontforge ../../scripts/fontconvert Greek-Regular.sfd --remove-overlap --otf
-
-
+cd ../sources/sfd/;
+ls *sfd > sfd.txt;
+echo 'hello' | awk '{print "fontforge ../../scripts/fontconvert "$0" --remove-overlap --otf"}' sfd.txt > sfdlist.sh;
+rm sfd.txt;
+sh sfdlist.sh
+rm sfdlist.sh
 rm ../../tests/fonts/*otf
 mv *otf ../../tests/fonts
 cd ../../tests/fonts
 git add .
 git commit -m 'Minor Updates to test'
 cp *otf /Library/Application\ Support/Adobe/Fonts
+echo 'DONE!';
