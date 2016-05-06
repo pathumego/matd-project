9091ca3d42bc76a5da6ebc1602c27821778b9009
Pathum Egodawatta <pathumego@gmail.com>
2016-05-06 14:41:23 +0100

    ALL COMMIT dump

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -2,7 +2,7 @@
 #Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
 cd ../sources/sfd/;
 ls *sfd > sfd.txt;
-echo 'hello' | awk '{print "fontforge ../../scripts/fontconvert "$0" --remove-overlap --otf"}' sfd.txt > sfdlist.sh;
+echo 'hello' | awk '{print "fontforge ../../scripts/fontconvert "$0" --remove-overlap --unlink-reference --otf"}' sfd.txt > sfdlist.sh;
 rm sfd.txt;
 sh sfdlist.sh
 rm sfdlist.sh
@@ -12,4 +12,5 @@ cd ../../tests/fonts
 git add .
 git commit -m 'Minor Updates to test'
 cp *otf /Library/Application\ Support/Adobe/Fonts
+cp *otf ../font-tester/tester/fonts
 echo 'DONE!';
