831fae7b5532f9c9c888db59c2dbbeea4d25149d
Pathum Egodawatta <pathumego@gmail.com>
2016-05-26 10:44:35 +0100

    Many adjustments and fixes.... Redesignd I mantra combinations and Halant combinations.

---
+++ sources/sfd/sfdlist.sh
@@ -0,0 +1,11 @@
+#fontforge ../../scripts/fontconvert experiment-latin-0.sfd --remove-overlap --unlink-reference --otf
+#fontforge ../../scripts/fontconvert experiment-latin-1.sfd --remove-overlap --unlink-reference --otf
+fontforge ../../scripts/fontconvert experiment-sinhala-0.sfd --remove-overlap --unlink-reference --otf
+#fontforge ../../scripts/fontconvert experiment-sinhala-1.sfd --remove-overlap --unlink-reference --otf
+fontforge ../../scripts/fontconvert experiment-sinhala-cursive-0.sfd --remove-overlap --unlink-reference --otf
+fontforge ../../scripts/fontconvert experiment-sinhala-cursive-1.sfd --remove-overlap --unlink-reference --otf
+#fontforge ../../scripts/fontconvert experiment-sinhala-italic-0.sfd --remove-overlap --unlink-reference --otf
+#fontforge ../../scripts/fontconvert experiment-tamil-0.sfd --remove-overlap --unlink-reference --otf
+#fontforge ../../scripts/fontconvert experiment-tamil-italic-0.sfd --remove-overlap --unlink-reference --otf
+#fontforge ../../scripts/fontconvert experiment-thaana-0.sfd --remove-overlap --unlink-reference --otf
+#fontforge ../../scripts/fontconvert experiment-thaana-1.sfd --remove-overlap --unlink-reference --otf
